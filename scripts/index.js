868f3954266e145a6e822712295d196a63550c1d
MarthaNava <navamartha.e@gmail.com>
2025-10-21 22:42:55 -0400

    Add smooth modal transitions and button hover effects

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,21 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form")
+const editProfileNameInput =editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput =editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 console.log({ newPostBtn, newPostModal, newPostCloseBtn });
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent
+    editProfileDescriptionInput.value = profileDescriptionEl.textContent
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +30,34 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt){
+  evt.preventDefault();
+  profileNameEl.textContent  = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const linkInput    = addCardFormElement.querySelector("#card-image-input");
+const captionInput = addCardFormElement.querySelector("#caption-input");
+
+newPostBtn.addEventListener("click", () => {
+  linkInput.value = "https://unsplash.com/example.jpg";
+  captionInput.value = "Val Thorens";
+  newPostModal.classList.add("modal_is-opened");
+});
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  console.log("Image link:", linkInput.value);
+  console.log("Caption:", captionInput.value);
+
+  newPostModal.classList.remove("modal_is-opened");
+  addCardFormElement.reset();
+}
+
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
