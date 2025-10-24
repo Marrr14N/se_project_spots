a1d983615bd21dcb90211ce9fa5b108b301f7eef
MarthaNava <navamartha.e@gmail.com>
2025-10-24 15:12:15 -0400

    corrections updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,25 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const editProfileForm = editProfileModal.querySelector(".modal__form")
-const editProfileNameInput =editProfileModal.querySelector("#profile-name-input");
-const editProfileDescriptionInput =editProfileModal.querySelector("#profile-description-input");
+const editProfileForm = document.forms[".modal__form"];
+document.querySelector;
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-console.log({ newPostBtn, newPostModal, newPostCloseBtn });
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
-  editProfileNameInput.value = profileNameEl.textContent
-    editProfileDescriptionInput.value = profileDescriptionEl.textContent
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -31,9 +35,9 @@ newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
 
-function handleEditProfileSubmit(evt){
+function handleEditProfileSubmit(evt) {
   evt.preventDefault();
-  profileNameEl.textContent  = editProfileNameInput.value;
+  profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
   editProfileModal.classList.remove("modal_is-opened");
 }
@@ -41,12 +45,10 @@ function handleEditProfileSubmit(evt){
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 const addCardFormElement = newPostModal.querySelector(".modal__form");
-const linkInput    = addCardFormElement.querySelector("#card-image-input");
+const linkInput = addCardFormElement.querySelector("#card-image-input");
 const captionInput = addCardFormElement.querySelector("#caption-input");
 
 newPostBtn.addEventListener("click", () => {
-  linkInput.value = "https://unsplash.com/example.jpg";
-  captionInput.value = "Val Thorens";
   newPostModal.classList.add("modal_is-opened");
 });
 
